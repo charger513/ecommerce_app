c89a614e7ebd528ba1a07be6c1707b36fd7aeb4e
Emmanuel Chargoy <charger513@hotmail.com>
2022-05-04 17:00:31 -0500

    Using the authStateChangesProvider in HomeAppBar

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -44,7 +44,7 @@ final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
   return auth;
 });
 
-final authStateProvider = StreamProvider.autoDispose<AppUser?>((ref) {
+final authStateChangesProvider = StreamProvider.autoDispose<AppUser?>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return authRepository.authStateChanges();
 });
