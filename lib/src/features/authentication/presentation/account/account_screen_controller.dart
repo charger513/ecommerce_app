9fe5ba3b20543c57f89c59397025a4de83ff61a0
Emmanuel Chargoy <charger513@hotmail.com>
2022-05-04 16:50:00 -0500

    Adding an AsyncValue extension method

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,9 +1,9 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+class AccountScreenController extends StateNotifier<AsyncValue> {
   AccountScreenController({required this.authRepository})
-      : super(const AsyncValue<void>.data(null));
+      : super(const AsyncValue.data(null));
 
   final FakeAuthRepository authRepository;
 
@@ -14,23 +14,24 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
     // if error, set state to error
 
     // try {
-    //   state = const AsyncValue<void>.loading();
+    //   state = const AsyncValue.loading();
     //   await authRepository.signOut();
-    //   state = const AsyncValue<void>.data(null);
+    //   state = const AsyncValue.data(null);
     //   return true;
     // } catch (e) {
-    //   state = AsyncValue<void>.error(e);
+    //   state = AsyncValue.error(e);
     //   return false;
     // }
 
-    state = const AsyncValue<void>.loading();
+    state = const AsyncValue.loading();
     state = await AsyncValue.guard(() => authRepository.signOut());
     return state.hasError == false;
   }
 }
 
-final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
-    AccountScreenController, AsyncValue<void>>((ref) {
+final accountScreenControllerProvider =
+    StateNotifierProvider.autoDispose<AccountScreenController, AsyncValue>(
+        (ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return AccountScreenController(authRepository: authRepository);
 });
