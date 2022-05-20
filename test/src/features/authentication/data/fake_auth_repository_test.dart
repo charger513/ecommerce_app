4a9ba6e0d3b4aded23b240c91b21abe8e5a8af3b
Emmanuel Chargoy <charger513@hotmail.com>
2022-05-20 15:59:47 -0500

    Testing the FakeAuthRepository (part 2) + advanced stream matchers

--- test/src/features/authentication/data/fake_auth_repository_test.dart
+++ test/src/features/authentication/data/fake_auth_repository_test.dart
@@ -22,9 +22,46 @@ void main() {
 
     test('currentUser is not null after sign in', () async {
       final authRepository = makeAuthRepository();
-      await authRepository.signInWithEmailAndPassword(testEmail, testPassword);
+      await authRepository.signInWithEmailAndPassword(
+        testEmail,
+        testPassword,
+      );
       expect(authRepository.currentUser, testUser);
       expect(authRepository.authStateChanges(), emits(testUser));
+    });
+
+    test('currentUser is not null after registration', () async {
+      final authRepository = makeAuthRepository();
+      await authRepository.createUserWithEmailAndPassword(
+        testEmail,
+        testPassword,
+      );
+      expect(authRepository.currentUser, testUser);
+      expect(authRepository.authStateChanges(), emits(testUser));
+    });
+
+    test('currentUser is null after sign out', () async {
+      final authRepository = makeAuthRepository();
+      await authRepository.signInWithEmailAndPassword(
+        testEmail,
+        testPassword,
+      );
+      expect(authRepository.currentUser, testUser);
+      expect(authRepository.authStateChanges(), emits(testUser));
+
+      await authRepository.signOut();
+      expect(authRepository.currentUser, null);
+      expect(authRepository.authStateChanges(), emits(null));
+    });
+
+    test('sign in after dispose throws exception', () {
+      final authRepository = makeAuthRepository();
+      authRepository.dispose();
+
+      expect(
+          () => authRepository.signInWithEmailAndPassword(
+              testEmail, testPassword),
+          throwsStateError);
     });
   });
 }
