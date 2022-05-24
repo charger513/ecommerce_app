f4da560ff6b7f47f6afdbaa71d5b8b2421cc7f7c
Emmanuel Chargoy <charger513@hotmail.com>
2022-05-24 18:29:53 -0500

    Testing the AccountScreenController (part 1) + AsyncValue subclasses

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,9 +1,9 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue> {
+class AccountScreenController extends StateNotifier<AsyncValue<void>> {
   AccountScreenController({required this.authRepository})
-      : super(const AsyncValue.data(null));
+      : super(const AsyncData(null));
 
   final FakeAuthRepository authRepository;
 
@@ -23,14 +23,13 @@ class AccountScreenController extends StateNotifier<AsyncValue> {
     //   return false;
     // }
 
-    state = const AsyncValue.loading();
+    state = const AsyncLoading();
     state = await AsyncValue.guard(() => authRepository.signOut());
   }
 }
 
-final accountScreenControllerProvider =
-    StateNotifierProvider.autoDispose<AccountScreenController, AsyncValue>(
-        (ref) {
+final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
+    AccountScreenController, AsyncValue<void>>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return AccountScreenController(authRepository: authRepository);
 });
