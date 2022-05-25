53e5cfabdd63eab086fb4fae58b4f9751dbcb9c3
Emmanuel Chargoy <charger513@hotmail.com>
2022-05-25 10:31:19 -0500

    Testing the AccountScreenController (part 2) + working with mocks

--- test/src/features/authentication/presentation/account/account_screen_controller_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -2,15 +2,31 @@ import 'package:ecommerce_app/src/features/authentication/data/fake_auth_reposit
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements FakeAuthRepository {}
 
 void main() {
   group('AccountScreenController', () {
-    test('initial state is ASyncValue.data', () {
-      final authRepository = FakeAuthRepository();
+    test('initial state is AsyncValue.data', () {
+      final authRepository = MockAuthRepository();
       final controller = AccountScreenController(
         authRepository: authRepository,
       );
+      verifyNever(authRepository.signOut);
       expect(controller.debugState, const AsyncData<void>(null));
+    });
+
+    test('signOut success', () async {
+      final authRepository = MockAuthRepository();
+      when(authRepository.signOut).thenAnswer((_) => Future.value());
+      final controller = AccountScreenController(
+        authRepository: authRepository,
+      );
+
+      await controller.signOut();
+      expect(controller.debugState, const AsyncData<void>(null));
+      verify(authRepository.signOut).called(1);
     });
   });
 }
