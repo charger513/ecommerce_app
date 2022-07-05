99b0d308951db58891315a4e96e9e656453c886f
Emmanuel Chargoy <charger513@hotmail.com>
2022-07-05 13:42:07 -0500

    Adding the email and password widget tests (part 2)

--- test/src/features/authentication/presentation/sign_in/email_password_sign_in_screen_test.dart
+++ test/src/features/authentication/presentation/sign_in/email_password_sign_in_screen_test.dart
@@ -31,5 +31,36 @@ void main() {
         () => authRepository.signInWithEmailAndPassword(any(), any()),
       );
     });
+
+    testWidgets('''
+      Given formType is signIn
+      When enter valid email and password
+      And tap on the sign-in button
+      Then signInWithEmailAndPassword is called
+      And onSignedIn callback is called
+      And error alert is not shown
+      ''', (tester) async {
+      var didSignIn = false;
+      final r = AuthRobot(tester);
+      when(() => authRepository.signInWithEmailAndPassword(
+          testEmail, testPassword)).thenAnswer((_) => Future.value());
+
+      await r.pumpEmailPasswordSignInContents(
+        authRepository: authRepository,
+        formType: EmailPasswordSignInFormType.signIn,
+        onSignedIn: () => didSignIn = true,
+      );
+
+      await r.enterEmail(testEmail);
+      await r.enterPassword(testPassword);
+      await r.tapEmailPasswordSubmitButton();
+
+      verify(
+        () =>
+            authRepository.signInWithEmailAndPassword(testEmail, testPassword),
+      ).called(1);
+      r.expectErrorAlertNotFound();
+      expect(didSignIn, true);
+    });
   });
 }
